9a77f70b59cf34936536bea3d54367ba4d8d3486
Davidchuck <rawlencendirangu@gmail.com>
2023-08-03 21:27:44 +0300

    first commit

---
+++ print.py
@@ -0,0 +1,32 @@
+#My first code, why always hello world
+#print ("My name is David")
+#print ("I'm 27 years old")
+#print ("Todays expectation's were to confirm my configurations are okay and successfully run first program")
+
+#Variables
+
+# Variables to store things you love
+thing1 = "Books"
+thing2 = "Traveling"
+
+# Variables to store reasons to learn programming
+reason1 = "To solve real-world problems"
+reason2 = "To create innovative solutions"
+
+# Variables to store things that give you motivation
+motivation1 = "Helping others"
+motivation2 = "Continuous learning"
+
+# Displaying the values
+print("Things I love:")
+print(thing1)
+print(thing2)
+
+print("\nReasons why I want to learn programming:")
+print(reason1)
+print(reason2)
+
+print("\nThings that give me motivation:")
+print(motivation1)
+print(motivation2)
+
