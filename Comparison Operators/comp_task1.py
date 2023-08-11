ac2b8cc3b580649f25070e42dba321b7de30faf1
Davidchuck <rawlencendirangu@gmail.com>
2023-08-11 23:00:08 +0300

    operators

---
+++ Comparison Operators/comp_task1.py
@@ -0,0 +1,65 @@
+#TASK 01
+#7+5 >= 10
+
+x=7
+y=5
+z=10
+
+xy=x + y
+
+print("It's", xy>=z, "7+5 >= 10")
+
+#“Anil” > “anil”
+
+x="Anil"
+y="anil"
+
+print("It's", x > y, "“Anil” > “anil”")
+
+#25 % 3 == 2
+x=25
+y=3
+z=2
+
+xy=x%y
+
+print("It's", xy == y, "25 % 3 == 2")
+
+#23 // 7  >= 9
+x=23//7
+y=9
+
+print("It's", x >= y, "23 // 7  >= 9")
+
+#7 + 3 % 2 != True
+x=7
+y=3
+z=2
+xy=x+y
+xyz=xy%2
+p=True
+
+
+print("It's", xyz != p, "7 + 3 % 2 != True")
+
+#False == False
+
+x=False
+y=False
+
+print("It's", x == y, "False == False")
+
+#True != False
+
+x=True
+y=False
+
+print("It's", x != y, "True != False")
+
+#29 == 89 % 30
+x=29
+y=89
+z=30
+yz=y%z
+
+print("it's", x == yz, "29 == 89 % 30" )
