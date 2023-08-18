ef43b6783209b4a759778390df7c044afcc05303
Davidchuck <rawlencendirangu@gmail.com>
2023-08-18 08:36:41 +0300

    if statement examples

---
+++ If Statements/if_statement.py
@@ -0,0 +1,16 @@
+#Simple if statement
+#Sytax
+
+#first example
+var_age=13
+
+#if section
+#if(var_age>=18):
+   # print("Allow into the party")
+#print("Go home, you are underage")
+    
+    #Example 2
+var_cloth="silk"
+if(var_cloth=="cotton"):
+    print("This is the right quality")
+print("This is too soft")
