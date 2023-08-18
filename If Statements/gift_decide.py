23ab92322dbccbf821edc178dda30ef11b8d0307
Davidchuck <rawlencendirangu@gmail.com>
2023-08-19 02:57:29 +0300

    gift decision assignment

---
+++ If Statements/gift_decide.py
@@ -0,0 +1,15 @@
+#code to decide the right gift to someone if they impressed or just want to gift them
+
+impressed="No"
+I_want_to_gift=("no")
+#print("What is the one thing you wouldn't consider a nice gift ")
+#dislike=input()
+print("What is one thing that you consider a valuable gift")
+likes=input()
+
+if(impressed=="Yes" or I_want_to_gift=="Yes"):
+    print("You deserve", likes ,"for a gift")
+else:
+    print("Sorry im not gifting you baecause I dont want to and you haven't impressed me") 
+
+
