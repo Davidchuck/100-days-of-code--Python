7714586aea9c9d58a5ccc6e34ee7bf060615e7f8
Davidchuck <rawlencendirangu@gmail.com>
2023-08-18 08:52:54 +0300

    budget check

---
+++ If Statements/Assignment1.py
@@ -0,0 +1,14 @@
+#Set the budget
+
+print("Whats your budget today")
+budget=float(input())
+
+#budget=500
+
+#Check quantity of items to buy
+if(budget >=5000):
+    #specify quantity to buy from the budget
+    print("You can buy 5 or more items in the mall")
+
+else:
+    print("This is a low budget, You can only buy less than 5 items from the Mall")
