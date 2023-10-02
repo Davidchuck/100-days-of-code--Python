912f46e454991a19116efb9cb2589157e07541ce
Davidchuck <rawlencendirangu@gmail.com>
2023-10-02 21:30:46 +0300

    array

---
+++ Data Structures/arrays.py
@@ -0,0 +1,12 @@
+#using arrays in data structures
+#Arrays stores data in one data type
+#Syntax
+'''array_name=arr.array(data_type, {array})'''
+
+#Code starts here
+
+#importing array liblary
+import array as r
+array_B=r.array("i", [24,6,21] )
+#test_array.append
+print("Test Array: ", array_B)
